6a56a086f319e5afee57c5a8a0d68b036be91522
Bryan Hoang <bryan.c.hoang@gmail.com>
2020-12-21 23:28:03 -0500

    fix(install.sh): fix up isntall.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,17 @@
 # -*- coding: utf-8 -*-
 
 git clone --bare git@github.com:bryan-hoang/dotfiles.git "${HOME}"/config
+
 config() {
   /usr/bin/git --git-dir="${HOME}"/config/ --work-tree="${HOME}" "$@"
 }
+
 mkdir -p config-backup
-if config checkout = 0; then
+if config checkout; then
   echo "Checked out config."
 else
   echo "Backing up pre-existing dot files."
-  config checkout 2>&1 | grep -E "\s+\." | awk "print \$1" | xargs -I{} mv {} .config-backup/{}
+  config checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs -I{} mv {} .config-backup/{}
 fi
 config checkout
 config config status.showUntrackedFiles no
