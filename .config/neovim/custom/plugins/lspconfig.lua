4a07eb894974d4fcebaf01202e3e62231fac960f
Bryan Hoang <bryan@bryanhoang.dev>
2022-10-11 16:25:03 -0400

    feat(nvim): add tsserver lsp for js/ts

--- .config/neovim/custom/plugins/lspconfig.lua
+++ .config/neovim/custom/plugins/lspconfig.lua
@@ -3,7 +3,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
-local servers = { "bashls", "sumneko_lua", "taplo"}
+local servers = { "bashls", "sumneko_lua", "taplo", "tsserver" }
 
 for _, lsp in ipairs(servers) do
 	lspconfig[lsp].setup({
