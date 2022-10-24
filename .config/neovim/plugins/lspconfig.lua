1edca31ce5bed45b9821b0744282e838842c6299
Bryan Hoang <bryan@bryanhoang.dev>
2022-10-24 07:51:43 -0400

    fix(nvim/config): fix diagnostics for undefined global symbols

--- .config/neovim/plugins/lspconfig.lua
+++ .config/neovim/plugins/lspconfig.lua
@@ -33,6 +33,23 @@ for _, lsp in ipairs(servers) do
 				},
 			},
 		}
+	elseif lsp ~= "sumneko_lua" then
+		setup_config["settings"] = {
+			Lua = {
+				runtime = {
+					-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+					version = "LuaJIT",
+				},
+				diagnostics = {
+					-- Get the language server to recognize the `vim` global
+					globals = { "vim" },
+				},
+				workspace = {
+					-- Make the server aware of Neovim runtime files
+					library = vim.api.nvim_get_runtime_file("", true),
+				},
+			},
+		}
 	end
 
 	lspconfig[lsp].setup(setup_config)
