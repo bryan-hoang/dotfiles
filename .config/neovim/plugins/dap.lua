bcba92e93c1ed2d62bd16f4c78e5a8a6bd5842ed
Bryan Hoang <bryan@bryanhoang.dev>
2022-11-04 10:07:17 -0400

    feat(nvim): add support for debugging bash scripts

--- .config/neovim/plugins/dap.lua
+++ .config/neovim/plugins/dap.lua
@@ -1,6 +1,8 @@
 return function()
+	local dap = require("dap")
+
 	for _, language in ipairs({ "typescript", "javascript" }) do
-		require("dap").configurations[language] = {
+		dap.configurations[language] = {
 			{
 				type = "pwa-node",
 				request = "launch",
@@ -10,4 +12,41 @@ return function()
 			},
 		}
 	end
+
+	-- Set up bashdb. Sources:
+	-- https://github.com/williamboman/mason.nvim/discussions/539
+	-- https://github.com/williamboman/mason.nvim/issues/578
+
+	-- Hardcoding the path since resolving it using the `mason-core.package`
+	-- module lead to some issues.
+	local bash_debug_adapter_install_path = vim.fn.stdpath("data")
+		.. "/mason/packages/bash-debug-adapter"
+	local bashdb_dir = bash_debug_adapter_install_path
+		.. "/extension"
+		.. "/bashdb_dir"
+
+	dap.adapters.sh = {
+		type = "executable",
+		command = "bash-debug-adapter",
+	}
+
+	dap.configurations.sh = {
+		{
+			name = "Launch bash debugger",
+			type = "sh",
+			request = "launch",
+			program = "${file}",
+			cwd = "${fileDirname}",
+			pathBashdb = bashdb_dir .. "/bashdb",
+			pathBashdbLib = bashdb_dir,
+			pathBash = "bash",
+			pathCat = "cat",
+			pathMkfifo = "mkfifo",
+			pathPkill = "pkill",
+			env = {},
+			args = {},
+			showDebugOutput = false,
+			trace = false,
+		},
+	}
 end
