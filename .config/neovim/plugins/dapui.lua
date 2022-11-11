04a5fea27b3c9759779a494278ba1ffebdd041aa
Bryan Hoang <bryan@bryanhoang.dev>
2022-11-11 14:53:48 -0500

    fix(nvim): prevent loading dapui twice
    Now a confusing message won't appear when running Packer related
    commands

--- .config/neovim/plugins/dapui.lua
+++ .config/neovim/plugins/dapui.lua
@@ -5,8 +5,15 @@ return {
 	config = function()
 		local dap = require("dap")
 		local dapui = require("dapui")
+		local UIState = require("dapui.state")
 
-		dapui.setup()
+		-- Prevent "Setup called twice" message from appearing when calling
+		-- :PackerSync multiple times.
+		-- https://github.com/rcarriga/nvim-dap-ui/blob/master/lua/dapui/init.lua#L133
+		local ui_state = UIState()
+		if not ui_state then
+			dapui.setup()
+		end
 
 		-- Using nvim-dap events to open and close the windows automatically.
 		dap.listeners.after.event_initialized["dapui_config"] = function()
