ede8bb05e4e348624ce075b0fa128f5c5efe5259
Bryan Hoang <bryan@bryanhoang.dev>
2022-11-28 09:30:53 -0500

    fix(nvim): fix presence button error
    Check if the `repo_url` for a file is `nil` before trying to display a
    button, otherwise an annoying error message appears. e.g., occurs with
    `man man` when using `nvim` in `MANPAGER`.

--- .config/neovim/plugins/presence.lua
+++ .config/neovim/plugins/presence.lua
@@ -2,14 +2,16 @@ return {
 	config = function()
 		local presence = require("presence")
 		presence:setup({
+			-- Enable debug logs.
 			log_level = nil,
+			-- function(buffer: string, repo_url: string | nil): table
 			buttons = function(_buffer, repo_url)
 				local button = {
 					label = "GitHub Profile",
 					url = "https://github.com/bryan-hoang",
 				}
 
-				if repo_url:match("dotfiles") then
+				if repo_url ~= nil and repo_url:match("dotfiles") then
 					button.label = "Workflow/Ricing"
 					button.url = "https://www.reddit.com/r/unixporn/"
 				end
