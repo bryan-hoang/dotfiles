86216134a652dcdab473e25f82aa68fcb7b93127
Bryan Hoang <bryan@bryanhoang.dev>
2023-01-06 09:10:40 -0500

    feat(nvim): add jump to prev line in file

--- .config/neovim/after/plugin/keymap/init.lua
+++ .config/neovim/after/plugin/keymap/init.lua
@@ -12,3 +12,16 @@ vim.keymap.set(
 	"<Cmd>nohlsearch<Bar>diffupdate<Bar>normal!<C-L><CR>",
 	{ noremap = false }
 )
+
+-- Automatically jump to the last place visited in a file before exiting.
+--
+-- https://this-week-in-neovim.org/2023/Jan/02#tips
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
