a307db63c69bef015dcaf9190296b55337839704
Bryan Hoang <bryan@bryanhoang.dev>
2023-01-24 01:17:13 -0500

    feat: add motd to ssh
    Also changed env/sh to be posix compliant.

--- .config/shell/env.sh
+++ .config/shell/env.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 export XDG_LOCAL_HOME="$HOME"/.local
 export XDG_BIN_HOME="$XDG_LOCAL_HOME"/bin
@@ -21,29 +21,29 @@ export ASDF_CONFIG_FILE="$XDG_CONFIG_HOME"/asdf/asdfrc
 
 # Searched last
 
-[[ -d "$XDG_CONFIG_HOME"/rofi/scripts ]] \
+[ -d "$XDG_CONFIG_HOME"/rofi/scripts ] \
 	&& export PATH="$XDG_CONFIG_HOME"/rofi/scripts:"$PATH"
-[[ -d "$XDG_DATA_HOME"/google-cloud-sdk/bin ]] \
+[ -d "$XDG_DATA_HOME"/google-cloud-sdk/bin ] \
 	&& export PATH="$XDG_DATA_HOME"/google-cloud-sdk/bin:"$PATH"
 # Brew
-[[ -s /home/linuxbrew/.linuxbrew/bin/brew ]] \
+[ -s /home/linuxbrew/.linuxbrew/bin/brew ] \
 	&& eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-[[ -s "$HOME"/.linuxbrew/bin/brew ]] \
+[ -s "$HOME"/.linuxbrew/bin/brew ] \
 	&& eval "$("$HOME"/.linuxbrew/bin/brew shellenv)"
 # JS eCoSyTeM
-[[ -d "$BUN_INSTALL"/bin ]] \
+[ -d "$BUN_INSTALL"/bin ] \
 	&& export PATH="$BUN_INSTALL"/bin:"$PATH"
-[[ -d "$DENO_INSTALL"/bin ]] \
+[ -d "$DENO_INSTALL"/bin ] \
 	&& export PATH="$DENO_INSTALL"/bin:"$PATH"
-[[ -d $PNPM_HOME ]] \
+[ -d "$PNPM_HOME" ] \
 	&& export PATH="$PNPM_HOME":"$PATH"
-[[ -d $GOPATH/bin ]] \
+[ -d "$GOPATH"/bin ] \
 	&& export PATH="$GOPATH"/bin:"$PATH"
 # shellcheck disable=SC1091
-[[ -s "$CARGO_HOME"/env ]] && . "$CARGO_HOME"/env
-[[ -d "$ASDF_DIR"/bin ]] \
+[ -s "$CARGO_HOME"/env ] && . "$CARGO_HOME"/env
+[ -d "$ASDF_DIR"/bin ] \
 	&& export PATH="$ASDF_DIR"/bin:"$PATH"
-[[ -d "$ASDF_DATA_DIR"/shims ]] \
+[ -d "$ASDF_DATA_DIR"/shims ] \
 	&& export PATH="$ASDF_DATA_DIR"/shims:"$PATH"
 export PATH="$XDG_BIN_HOME":"$PATH"
 
@@ -54,14 +54,18 @@ export XCURSOR_THEME=Dracula-cursors
 export XCURSOR_DISCOVER=1
 
 # Default editor.
-if command -v nvim &>/dev/null || [[ -f ${XDG_BIN_HOME}/nvim ]]; then
+if
+	command -v nvim >/dev/null 2>&1 || [ -f "$XDG_BIN_HOME"/nvim ]
+then
 	export EDITOR=nvim
-	export VISUAL=${EDITOR}
-	export SUDO_EDITOR=${EDITOR}
-elif command -v vim &>/dev/null; then
+	export VISUAL="$EDITOR"
+	export SUDO_EDITOR="$EDITOR"
+elif
+	command -v vim >/dev/null 2>&1
+then
 	export EDITOR=vim
-	export VISUAL=${EDITOR}
-	export SUDO_EDITOR=${EDITOR}
+	export VISUAL="$EDITOR"
+	export SUDO_EDITOR="$EDITOR"
 fi
 
 # Make Python use UTF-8 encoding for output to stdin, stdout, and stderr.
@@ -69,8 +73,8 @@ export PYTHONIOENCODING=UTF-8
 
 # Increase history size. The default is 500.
 export HISTSIZE=32768
-export HISTFILESIZE=${HISTSIZE}
-export SAVEHIST=${HISTSIZE}
+export HISTFILESIZE="$HISTSIZE"
+export SAVEHIST="$HISTSIZE"
 
 # Omit duplicates and commands that begin with a space from history.
 export HISTCONTROL='ignoreboth:erasedups'
@@ -79,7 +83,7 @@ export HISTCONTROL='ignoreboth:erasedups'
 export PAGER=less
 
 # Using Neovim as the man pager to make copying and following links easier.
-command -v nvim &>/dev/null \
+command -v nvim >/dev/null 2>&1 \
 	&& export MANPAGER='nvim +Man!'
 
 # Enable persistent REPL history for `node`.
@@ -87,7 +91,7 @@ export NODE_REPL_HISTORY="$XDG_STATE_HOME"/node/history
 mkdir -p "$(dirname "$NODE_REPL_HISTORY")"
 
 # Allow 32^3 entries; the default is 1000.
-export NODE_REPL_HISTORY_SIZE=${HISTSIZE}
+export NODE_REPL_HISTORY_SIZE="$HISTSIZE"
 
 # Use sloppy mode by default, matching web browsers.
 export NODE_REPL_MODE=sloppy
@@ -116,7 +120,7 @@ export GHQ_ROOT="$HOME"/src
 
 case ${TERM} in
 	# WSL in windows Terminal colour support.
-	xterm-256color)
+	xterm-256color|wezterm)
 		export COLORTERM=truecolor
 		;;
 	*) ;;
@@ -135,7 +139,7 @@ export EXA_STRICT=true
 
 export MCFLY_KEY_SCHEME=vim
 export MCFLY_RESULTS_SORT=LAST_RUN
-export MCFLY_HISTORY_LIMIT=${HISTSIZE}
+export MCFLY_HISTORY_LIMIT="$HISTSIZE"
 export MCFLY_RESULTS=20
 export MCFLY_INTERFACE_VIEW=BOTTOM
 export MCFLY_FUZZY=2
@@ -161,15 +165,15 @@ chmod 600 "$GNUPGHOME"/*
 chmod 700 "$GNUPGHOME"
 export PASSWORD_STORE_DIR="$XDG_DATA_HOME"/pass
 export INPUTRC="$XDG_CONFIG_HOME"/readline/inputrc
-export RIPGREP_CONFIG_PATH=${XDG_CONFIG_HOME}/ripgrep/config
+export RIPGREP_CONFIG_PATH="$XDG_CONFIG_HOME"/ripgrep/config
 export STARSHIP_CONFIG="$XDG_CONFIG_HOME"/starship.toml
 export STARSHIP_CACHE="$XDG_CACHE_HOME"/starship
 export WAKATIME_HOME="$XDG_CONFIG_HOME"/wakatime
 mkdir -p "$WAKATIME_HOME"
 export MYSQL_HISTFILE="$XDG_STATE_HOME"/mysql/history
 mkdir -p "$(dirname "$MYSQL_HISTFILE")"
-export MYSQL_HOME=$XDG_CONFIG_HOME/mysql
-export SQLITE_HISTORY=${XDG_STATE_HOME}/sqlite/history
+export MYSQL_HOME="$XDG_CONFIG_HOME"/mysql
+export SQLITE_HISTORY="$XDG_STATE_HOME"/sqlite/history
 mkdir -p "$(dirname "$SQLITE_HISTORY")"
 export WGETRC="$XDG_CONFIG_HOME"/wget/wgetrc
 
@@ -226,7 +230,7 @@ export FZF_DEFAULT_OPTS='--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 export TERMINFO="$XDG_DATA_HOME"/terminfo
 export TERMINFO_DIRS="$XDG_DATA_HOME"/terminfo:/usr/share/terminfo
-export RUBY_DEBUG_HISTORY_FILE=$XDG_STATE_HOME/rdbg/history
+export RUBY_DEBUG_HISTORY_FILE="$XDG_STATE_HOME"/rdbg/history
 mkdir -p "$(dirname "$RUBY_DEBUG_HISTORY_FILE")"
-export BUNDLE_USER_CACHE=$XDG_CACHE_HOME/bundle
-export BUNDLE_USER_CONFIG=$XDG_CONFIG_HOME/bundle/config
+export BUNDLE_USER_CACHE="$XDG_CACHE_HOME"/bundle
+export BUNDLE_USER_CONFIG="$XDG_CONFIG_HOME"/bundle/config
