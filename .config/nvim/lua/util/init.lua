3e13fcbdd82864c3f1290b83946c2028abdf969e
Bryan Hoang <bryan@bryanhoang.dev>
2023-02-23 10:01:53 -0500

    feat(nvim): add util to set file types

--- .config/nvim/lua/util/init.lua
+++ .config/nvim/lua/util/init.lua
@@ -3,9 +3,22 @@ local M = {}
 -- Returns a custom list of events that fire only when entering a buffer.
 function M.get_buf_enter_event_list()
 	return {
-		"BufReadPre",
+		"BufRead",
 		"BufNewFile",
 	}
+end
+
+-- Creates auto commands to set file types based on filename patterns.
+function M.set_file_associations(file_associations)
+	for file_type, patterns in pairs(file_associations) do
+		vim.api.nvim_create_autocmd({
+			"BufRead",
+			"BufNewFile",
+		}, {
+			command = "setfiletype " .. file_type,
+			pattern = patterns,
+		})
+	end
 end
 
 return M
