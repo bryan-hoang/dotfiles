160b98bb263f25553620408dbbe0765ec902baa4
Bryan Hoang <bryan@bryanhoang.dev>
2023-02-26 01:21:13 -0500

    feat(nvim): add file association autocmds

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local set_file_associations = require("util").set_file_associations
+
+set_file_associations({
+	["yaml"] = { "gemrc" },
+	["gitconfig"] = { "*gitconfig" },
+	["gitignore"] = { "*ignore" },
+	["nginx"] = { "/etc/nginx/**/*.conf" },
+	["sshconfig"] = { "**/.ssh/conf.d/*.conf" },
+	["jsonc"] = { "**/.config/markdownlint/config" },
+})
