0663e0ca9270b979e117522be81b9890dc001893
Bryan Hoang <bryan@bryanhoang.dev>
2023-03-05 17:20:43 -0500

    fix(nvim): don't install list of ts langauges

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,7 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		-- dependencies = { "windwp/nvim-ts-autotag" },
 		opts = {
+			ensure_installed = {},
 			-- Automatically install missing parsers when entering buffer
 			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
 			auto_install = true,
