f6b4411d74f4c588dea55cc5f8731397fdf52d2d
Bryan Hoang <bryan@bryanhoang.dev>
2023-03-13 17:50:02 -0400

    fix(nvim): don't lazy load vim-fetch plugin
    Now, file line and column numbers are read using the `:l:c` syntax.

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -3,6 +3,7 @@ return {
 		-- Make Vim handle line and column numbers in file names with a minimum of
 		-- fuss.
 		"wsdjeg/vim-fetch",
+		lazy = false,
 	},
 	{
 		"eandrju/cellular-automaton.nvim",
