f5a5c2e61e2be849ebea879b8bec33ebbadc7724
Bryan Hoang <bryan@bryanhoang.dev>
2023-03-20 23:24:05 -0400

    feat(wezterm): update cfg w/ hyperlink fix

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,19 +6,88 @@ local is_os_unix = string.sub(package.config, 1, 1) == "/"
 local zsh = { "zsh", "--interactive" }
 local bash = { "bash", "-i" }
 local git_bash = { "C:\\Program Files\\Git\\bin\\bash.exe", "--login" }
-local x_padding = 8
+
+-- Max window on startup.
+wezterm.on("gui-startup", function(cmd)
+	---@diagnostic disable-next-line: unused-local
+	local _tab, _pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
+
+local config = {
+	color_scheme = "Dracula (Official)",
+	font = wezterm.font(
+		is_os_unix and "FiraCode Nerd Font Mono" or "FiraCode NFM"
+	),
+	-- Default is 12.0.
+	font_size = 11.0,
+	default_cursor_style = "SteadyBlock",
+	use_fancy_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
+	-- http://lua-users.org/wiki/TernaryOperator
+	-- https://stackoverflow.com/a/14425862/8714233
+	default_prog = is_os_unix and zsh or git_bash,
+	window_background_opacity = 0.75,
+	window_close_confirmation = "NeverPrompt",
+	initial_cols = 120,
+	initial_rows = 32,
+	selection_word_boundary = " \t\n{}[]()\"'`",
+	enable_kitty_keyboard = true,
+	window_decorations = "RESIZE",
+}
+
+config.launch_menu = {
+	{
+		label = "Z shell (Zsh)",
+		args = zsh,
+	},
+	{
+		label = "bash",
+		args = bash,
+	},
+	{
+		label = "Git Bash",
+		args = git_bash,
+	},
+	{
+		label = "Powershell Core",
+		args = { "pwsh" },
+	},
+}
+
+config.keys = {
+	-- Activate the Launcher Menu in the current tab.
+	{ key = "l", mods = "ALT", action = wezterm.action.ShowLauncher },
+	-- Override default key binding to disable confirmation of closing the tab.
+	{
+		key = "W",
+		mods = "CTRL",
+		action = wezterm.action.CloseCurrentTab({ confirm = false }),
+	},
+}
+
+config.mouse_bindings = {
+	-- Change the default click behavior so that it only selects
+	-- text and doesn't open hyperlinks
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "NONE",
+		action = act.CompleteSelection("PrimarySelection"),
+	},
+	-- and make CTRL-Click open hyperlinks
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = act.OpenLinkAtMouseCursor,
+	},
+}
 
 local wsl_domains = wezterm.default_wsl_domains()
 for _index, domain in ipairs(wsl_domains) do
 	-- Otherwise, its opened in the Windows user's home directory.
 	domain.default_cwd = "~"
 end
-
-wezterm.on("gui-startup", function(cmd)
-	---@diagnostic disable-next-line: unused-local
-	local _tab, _pane, window = mux.spawn_window(cmd or {})
-	window:gui_window():maximize()
-end)
+config.wsl_domains = wsl_domains
 
 -- https://wezfurlong.org/wezterm/config/lua/wezterm/enumerate_ssh_hosts/
 local ssh_domains = {}
@@ -39,138 +108,29 @@ for host, _config in pairs(wezterm.enumerate_ssh_hosts()) do
 		assume_shell = "Posix",
 	})
 end
+config.ssh_domains = ssh_domains
 
-return {
-	color_scheme = "Dracula (Official)",
-	font = wezterm.font(
-		is_os_unix and "FiraCode Nerd Font Mono" or "FiraCode NFM"
-	),
-	-- Default is 12.0.
-	font_size = 11.0,
-	default_cursor_style = "SteadyBlock",
-	use_fancy_tab_bar = false,
-	hide_tab_bar_if_only_one_tab = true,
-	-- http://lua-users.org/wiki/TernaryOperator
-	-- https://stackoverflow.com/a/14425862/8714233
-	default_prog = is_os_unix and zsh or git_bash,
-	launch_menu = {
-		{
-			label = "Z shell (Zsh)",
-			args = zsh,
-		},
-		{
-			label = "bash",
-			args = bash,
-		},
-		{
-			label = "Git Bash",
-			args = git_bash,
-		},
-		{
-			label = "Powershell Core",
-			args = { "pwsh" },
-		},
-	},
-	wsl_domains = wsl_domains,
-	ssh_domains = ssh_domains,
-	window_background_opacity = 0.75,
-	window_close_confirmation = "NeverPrompt",
-	initial_cols = 120,
-	initial_rows = 32,
-	selection_word_boundary = " \t\n{}[]()\"'`",
-	mouse_bindings = {
-		-- Change the default click behavior so that it only selects
-		-- text and doesn't open hyperlinks
-		{
-			event = { Up = { streak = 1, button = "Left" } },
-			mods = "NONE",
-			action = act.CompleteSelection("PrimarySelection"),
-		},
-		-- and make CTRL-Click open hyperlinks
-		{
-			event = { Up = { streak = 1, button = "Left" } },
-			mods = "CTRL",
-			action = act.OpenLinkAtMouseCursor,
-		},
-	},
-	hyperlink_rules = {
-		-- Linkify things that look like URLs and the host has a TLD name in
-		-- punctionation (i.e., brackets). e.g.,
-		-- (https://example.com)
-		-- [https://example.com]
-		-- <https://example.com>
-		-- "https://example.com"
-		--
-		{
-			regex = "[[:punct:]](\\w+://\\S+)[[:punct:]]",
-			format = "$1",
-		},
-		-- Then handle URLs not wrapped in brackets
-		-- and include terminating ), / or - characters, if any
-		-- these seem to be the most common trailing characters that are part of URLs
-		-- there may be additional common ones. e.g.,
-		-- https://example.com/
-		-- https://www.rfc-editor.org/rfc/rfc2606.html#section-1
-		-- https://dcp.work/?bankAccount=foo
-		{
-			regex = "\\b\\w+://\\S+[)/a-zA-Z0-9-]+",
-			format = "$0",
-		},
-		-- Linkify email addresses (e.g., bryan@bryanhoang.dev)
-		-- Compiled-in default. Used if you don't specify any hyperlink_rules.
-		{
-			regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
-			format = "mailto:$0",
-		},
-		-- file:// URI
-		-- Compiled-in default. Used if you don't specify any hyperlink_rules.
-		{
-			regex = [[\bfile://\S*\b]],
-			format = "$0",
-		},
-		-- Linkify things that look like URLs with numeric addresses as hosts.
-		-- E.g. http://127.0.0.1:8000/ for a local development server,
-		-- or http://192.168.1.1/ for the web interface of many routers.
-		{
-			regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b/?]],
-			format = "$0",
-		},
-		-- Linkify URLs like http://[::1]:5000/ for local development. e.g., from
-		-- `dufs`.
-		{
-			regex = [[\b\w+://\[::\d\]\S*\b/?]],
-			format = "$0",
-		},
-		-- Match http://localhost:1337/.
-		{
-			regex = [[http://localhost:[0-9]+\S*\b/?]],
-			format = "$0",
-		},
-		-- Make username/project paths clickable. This implies paths like the following are for GitHub.
-		-- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
-		-- As long as a full URL hyperlink regex exists above this it should not match a full URL to
-		-- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
-		{
-			regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
-			format = "https://www.github.com/$1/$3",
-		},
-	},
-	enable_kitty_keyboard = true,
-	keys = {
-		-- Activate the Launcher Menu in the current tab.
-		{ key = "l", mods = "ALT", action = wezterm.action.ShowLauncher },
-		-- Override default key binding to disable confirmation of closing the tab.
-		{
-			key = "W",
-			mods = "CTRL",
-			action = wezterm.action.CloseCurrentTab({ confirm = false }),
-		},
-	},
-	window_padding = {
-		left = x_padding,
-		right = x_padding,
-		top = 0,
-		bottom = 0,
-	},
-	window_decorations = "RESIZE",
+local x_padding = 8
+config.window_padding = {
+	left = x_padding,
+	right = x_padding,
+	top = 0,
+	bottom = 0,
 }
+
+-- https://wezfurlong.org/wezterm/config/lua/config/hyperlink_rules.html?h=hyperlink
+--
+-- Use the defaults as a base
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+-- Make username/project paths clickable. this implies paths like the following
+-- are for github. ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim
+-- | wez/wezterm | "wez/wezterm.git" ) as long as a full url hyperlink regex
+-- exists above this it should not match a full url to github or gitlab /
+-- bitbucket (i.e. https://gitlab.com/user/project.git is still a whole
+-- clickable url)
+table.insert(config.hyperlink_rules, {
+	regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+	format = "https://www.github.com/$1/$3",
+})
+
+return config
