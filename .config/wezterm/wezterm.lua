5abe3344e3076551069f3f29931d0614b4ab34ef
Bryan Hoang <bryan@bryanhoang.dev>
2023-03-23 09:12:05 -0400

    fix(wezterm): fix hyperlink config insert

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -126,16 +126,18 @@ config.window_padding = {
 -- https://wezfurlong.org/wezterm/config/lua/config/hyperlink_rules.html?h=hyperlink
 --
 -- Use the defaults as a base
-config.hyperlink_rules = wezterm.default_hyperlink_rules()
+local hyperlink_rules = wezterm.default_hyperlink_rules()
 -- Make username/project paths clickable. this implies paths like the following
 -- are for github. ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim
 -- | wez/wezterm | "wez/wezterm.git" ) as long as a full url hyperlink regex
 -- exists above this it should not match a full url to github or gitlab /
 -- bitbucket (i.e. https://gitlab.com/user/project.git is still a whole
 -- clickable url)
-table.insert(config.hyperlink_rules, {
-	regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
-	format = "https://www.github.com/$1/$3",
+table.insert(hyperlink_rules, {
+	regex = [[([\w\d]{1}[-\w\d]+)(?:/){1}([-\w\d\.]+)]],
+	format = "https://github.com/$1/$2",
 })
+
+config.hyperlink_rules = hyperlink_rules
 
 return config
