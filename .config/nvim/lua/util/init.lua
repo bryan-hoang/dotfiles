fff73bc45c3620783ea61f34e8e15a074f08ab8c
Bryan Hoang <bryan@bryanhoang.dev>
2023-04-19 15:37:25 -0400

    fix(nvim): move ft fn back into util

--- .config/nvim/lua/util/init.lua
+++ .config/nvim/lua/util/init.lua
@@ -6,6 +6,20 @@ M.buf_enter_event_list = {
 	"BufNewFile",
 }
 
+local function set_file_associations(file_associations)
+	for file_type, patterns in pairs(file_associations) do
+		for _, pattern in ipairs(patterns) do
+			vim.filetype.add({
+				pattern = {
+					[pattern] = file_type,
+				},
+			})
+		end
+	end
+end
+
+M.set_file_associations = set_file_associations
+
 M.is_os_unix = string.sub(package.config, 1, 1) == "/"
 
 return M
