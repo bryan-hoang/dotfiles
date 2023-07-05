888be284b84c44629d46849d979cc03d5a68dfe1
Bryan Hoang <bryan@bryanhoang.dev>
2023-07-05 13:42:59 -0400

    feat(nvim): auto jump to last place in file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,5 @@
 -- https://github.com/neovim/neovim/pull/22668
+-- :h vim.loader.enable()
 vim.loader.enable()
 
 -- bootstrap lazy.nvim, LazyVim and your plugins
--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,18 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Automatically jump to the last place I’ve visited in a file before exiting.
+-- https://github.com/phaazon/this-week-in-neovim-contents/blob/master/contents/2023/Jan/02/5-did-you-know.md
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+
+		-- If the previous mark is still a valid line number in the file, set the
+		-- cursor.
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
