d7c1d116ffb93644507803f02e3fc4c7d877f8e8
Bryan Hoang <bryan@bryanhoang.dev>
2023-07-25 15:57:19 -0400

    feat(git): add orderfile for diff/mergetool

--- .config/nvim/filetype.lua
+++ .config/nvim/filetype.lua
@@ -3,7 +3,11 @@ local set_file_associations = require("util").set_file_associations
 set_file_associations({
 	yaml = { "gemrc", os.getenv("BUNDLE_USER_CONFIG") },
 	gitconfig = { ".*gitconfig" },
-	gitignore = { ".*ignore", ".*/default-pkgs/.*%.list" },
+	gitignore = {
+		".*ignore",
+		".*/default-pkgs/.*%.list",
+		".*/%.config/git/order",
+	},
 	nginx = { "/etc/nginx/.*/.*%.conf" },
 	sshconfig = { ".*/%.ssh/conf%.d/.*%.conf" },
 	jsonc = { ".*/%.config/markdownlint/config", ".*/%.vscode/.*%.json" },
