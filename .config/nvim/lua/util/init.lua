431507b9a77bc42caf339e0ec27658a27708f45d
Bryan Hoang <bryan@bryanhoang.dev>
2023-10-23 08:44:00 -0400

    refactor(nvim): reorder export for util fn

--- .config/nvim/lua/util/init.lua
+++ .config/nvim/lua/util/init.lua
@@ -6,7 +6,7 @@ M.buf_enter_event_list = {
 	"BufNewFile",
 }
 
-local function set_file_associations(file_associations)
+M.set_file_associations = function(file_associations)
 	for file_type, patterns in pairs(file_associations) do
 		for _, pattern in ipairs(patterns) do
 			vim.filetype.add({
@@ -17,8 +17,6 @@ local function set_file_associations(file_associations)
 		end
 	end
 end
-
-M.set_file_associations = set_file_associations
 
 M.is_os_unix = string.sub(package.config, 1, 1) == "/"
 
