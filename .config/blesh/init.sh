a5d8f846b1b7ac5e883259dc67f7180b7ae370cb
Bryan Hoang <bryan@bryanhoang.dev>
2024-04-20 21:35:49 -0400

    fix(bleh.sh): disable exit marker

--- .config/blesh/init.sh
+++ .config/blesh/init.sh
@@ -4,6 +4,8 @@
 ble-face -s auto_complete fg=008
 # Disable error exit marker like "[ble: exit %d]"
 bleopt exec_errexit_mark=
+# Disable exit marker like "[ble: exit]"
+bleopt exec_exit_mark=
 # Disable elapsed-time marker like "[ble: elapsed 1.203s (CPU 0.4%)]"
 bleopt exec_elapsed_mark=
 # Change EOF marker from "[ble: EOF]"
