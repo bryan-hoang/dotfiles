804bc74a6458b12dc2153e9129ef69443d9d8081
Bryan Hoang <bryan@bryanhoang.dev>
2024-05-02 14:30:58 -0400

    perf(atuin): disable on zfs based file systems
    Refs: https://unix.stackexchange.com/questions/53313/how-to-show-the-filesystem-type-via-the-terminal

--- .config/shell/common.sh
+++ .config/shell/common.sh
@@ -111,7 +111,10 @@ does_command_exist register-python-argcomplete pipx \
 # endregion
 
 does_command_exist thefuck && eval "$(thefuck --alias)"
-does_command_exist atuin && eval "$(atuin init --disable-up-arrow "$SHELL_BASENAME")"
+# Disable atuin on zfs file systems. See
+# https://github.com/atuinsh/atuin/issues/952
+df --print-type /home | tail --lines=+2 | awk '{print $2}' | grep --quiet --invert-match zfs \
+	&& does_command_exist atuin && eval "$(atuin init --disable-up-arrow "$SHELL_BASENAME")"
 
 # https://wiki.archlinux.org/title/XDG_Base_Directory
 mkdir -p "$XDG_DATA_HOME"/tig
