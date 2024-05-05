ec7fb3a127deb1eba04a18bf5e15111909b14c40
Bryan Hoang <bryan@bryanhoang.dev>
2024-05-05 15:02:54 -0400

    feat(node): enable compile cache by default

--- .config/shell/env.sh
+++ .config/shell/env.sh
@@ -133,6 +133,7 @@ export NODE_REPL_HISTORY_SIZE="$HISTFILESIZE"
 # Use sloppy mode by default, matching web browsers.
 export NODE_REPL_MODE=sloppy
 export NODE_INSPECT_RESUME_ON_START=1
+export NODE_COMPILE_CACHE="$XDG_CACHE_HOME"/node/compile
 
 export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME"/npm/npmrc
 
