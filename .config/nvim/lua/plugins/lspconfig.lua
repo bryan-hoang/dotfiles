9f4582f8bcf9bb800c0649993e27d1e03b632fbf
Bryan Hoang <bryan@bryanhoang.dev>
2024-05-22 08:42:36 -0400

    fix(nvim): disable codelens by default
    Otherwise a lot of warning messages can be printed

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -41,10 +41,6 @@ return {
 				-- Be aware that you also will need to properly configure your LSP
 				-- server to provide the inlay hints.
 				inlay_hints = { enabled = true },
-				-- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
-				-- Be aware that you also will need to properly configure your LSP
-				-- server to provide the code lenses.
-				codelens = { enabled = true },
 				format = { async = true },
 				---@type lspconfig.options
 				servers = {
