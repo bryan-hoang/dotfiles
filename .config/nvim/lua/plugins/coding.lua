7b949b8b923007b31b69966483e29051534d6303
Bryan Hoang <bryan@bryanhoang.dev>
2024-07-04 17:00:27 -0400

    refactor(nvim): update how git completions are set up

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -10,32 +10,29 @@ return {
 		event = require("util").buf_enter_event_list,
 	},
 	{
-		"petertriho/cmp-git",
-		dependencies = { "hrsh7th/nvim-cmp" },
-		ft = { "gitcommit", "octo", "markdown" },
-		config = function(_, opts)
-			local cmp = require("cmp")
-			local cmp_git = require("cmp_git")
-
-			cmp.setup(opts)
-			cmp.setup.filetype({ "gitcommit", "octo", "markdown" }, {
-				sources = {
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "buffer" },
-					{ name = "path" },
-					{ name = "git" },
-				},
-			})
-			cmp_git.setup()
+		"hrsh7th/nvim-cmp",
+		opts = function(_, opts)
+			opts.completion.autocomplete = false
 		end,
 	},
 	{
-		"hrsh7th/nvim-cmp",
-		opts = {
-			completion = {
-				autocomplete = false,
-			},
-		},
+		"petertriho/cmp-git",
+		dependencies = { "hrsh7th/nvim-cmp" },
+		ft = { "gitcommit", "octo", "markdown" },
+		config = function(_, _)
+			local cmp = require("cmp")
+			local cmp_git = require("cmp_git")
+
+			cmp.setup.filetype({ "gitcommit", "octo", "markdown" }, {
+				sources = cmp.config.sources({
+					{ name = "git" },
+					{ name = "path" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+
+			cmp_git.setup()
+		end,
 	},
 }
