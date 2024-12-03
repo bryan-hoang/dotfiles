19c0ecf2ee0af2bf8ec2ce839640f20efeefee49
Bryan Hoang <bryan@bryanhoang.dev>
2024-12-03 14:09:10 -0500

    perf(windows): disable ble.sh autocomplete to avoid laggy typing

--- .config/blesh/init.sh
+++ .config/blesh/init.sh
@@ -2,6 +2,7 @@
 
 # Change to match `zsh-autosuggestions`.
 ble-face -s auto_complete fg=008
+
 # Disable error exit marker like "[ble: exit %d]"
 bleopt exec_errexit_mark=
 # Disable exit marker like "[ble: exit]"
@@ -10,3 +11,7 @@ bleopt exec_exit_mark=
 bleopt exec_elapsed_mark=
 # Change EOF marker from "[ble: EOF]"
 bleopt prompt_eol_mark='⏎'
+# Disable auto-complete on windows due to perf issues searching PATH.
+if [ "$(uname -o)" = 'Msys' ]; then
+	bleopt complete_auto_complete=
+fi
