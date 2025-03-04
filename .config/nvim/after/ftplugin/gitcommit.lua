a8ee977a4ad3a2d2a98e28193cba6303112b28b6
Bryan Hoang <bryan@bryanhoang.dev>
2025-03-04 09:47:14 -0500

    fix(nvim): normalize tab stops when editing verbose commits messages
    Signed-off-by: Bryan Hoang <bryan@bryanhoang.dev>

--- .config/nvim/after/ftplugin/gitcommit.lua
+++ .config/nvim/after/ftplugin/gitcommit.lua
@@ -1,2 +1,5 @@
 -- Color recommended column limits for the commit header and body.
 vim.opt.colorcolumn = { 50, 72 }
+-- Render tabs consistently.
+-- https://stackoverflow.com/a/62237860/8714233
+vim.opt.ts = 2
