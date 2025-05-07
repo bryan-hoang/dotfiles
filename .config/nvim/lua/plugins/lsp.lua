761b7aa5386818e8defd5d96ee0108f874b02519
Bryan Hoang <bryan@bryanhoang.dev>
2025-05-07 09:46:24 -0400

    fix(nvim): pin mason plugins to v1
    Necessary until LazyVim is updated to address breaking changes in v2 of
    mason plugins.
    
    Refs: https://github.com/LazyVim/LazyVim/issues/6039

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -207,4 +207,7 @@ return {
 			return merged_opts
 		end,
 	},
+	-- FIXME: https://github.com/LazyVim/LazyVim/issues/6039
+	{ "mason-org/mason.nvim", version = "1" },
+	{ "mason-org/mason-lspconfig.nvim", version = "1" },
 }
