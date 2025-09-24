b4714f9bd951af4fa8a54b61cae46e469da9a33c
Bryan Hoang <bryan@bryanhoang.dev>
2025-09-24 10:58:45 -0400

    feat(nvim): add cflint linter

--- .config/nvim/lua/plugins/extras/lang/cf.lua
+++ .config/nvim/lua/plugins/extras/lang/cf.lua
@@ -6,11 +6,19 @@ return {
 			local function get_path_arg()
 				return "path=" .. vim.api.nvim_buf_get_name(0)
 			end
+			local function get_file_name()
+				return vim.api.nvim_buf_get_name(0)
+			end
 
-			local severities = {
+			local fixinatorSeverities = {
 				[1] = vim.diagnostic.severity.INFO,
 				[2] = vim.diagnostic.severity.WARN,
 				[3] = vim.diagnostic.severity.ERROR,
+			}
+			local cflintSeverities = {
+				INFO = vim.diagnostic.severity.INFO,
+				WARNING = vim.diagnostic.severity.WARN,
+				ERROR = vim.diagnostic.severity.ERROR,
 			}
 
 			local user_opts = {
@@ -38,7 +46,7 @@ return {
 								local diagnostic = {
 									lnum = result.line - 1,
 									col = result.column,
-									severity = severities[result.severity],
+									severity = fixinatorSeverities[result.severity],
 									message = result.description,
 									source = "fixinator",
 									code = result.id,
@@ -49,10 +57,48 @@ return {
 							return diagnostics
 						end,
 					},
+					cflint = {
+						cmd = "java",
+						stdin = true,
+						args = {
+							"-jar",
+							os.getenv("XDG_DATA_HOME") .. "/cflint/CFLint-1.5.0-all.jar",
+							"-e",
+							"-quiet",
+							"-json",
+							"-stdout",
+							"-stdin",
+							get_file_name,
+						},
+						parser = function(output)
+							local diagnostics = {}
+							local ok, decoded = pcall(vim.json.decode, output)
+
+							if not ok then
+								return diagnostics
+							end
+
+							for _, issue in ipairs(decoded.issues or {}) do
+								for _, location in ipairs(issue.locations) do
+									local diagnostic = {
+										lnum = location.line - 1,
+										col = location.column,
+										severity = cflintSeverities[issue.severity],
+										message = location.message,
+										source = "cflint",
+										code = issue.id,
+									}
+									table.insert(diagnostics, diagnostic)
+								end
+							end
+
+							return diagnostics
+						end,
+					},
 				},
 
 				linters_by_ft = {
-					cf = { "fixinator" },
+					cf = { "fixinator", "cflint" },
 				},
 			}
 
