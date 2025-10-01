2b06bc10fd8fd698cfd24943d66c5f10b8ffa3b0
Bryan Hoang <bryan@bryanhoang.dev>
2025-10-01 14:06:45 -0400

    fix(nvim): rm workaround for catppuccin breaking change

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -76,13 +76,4 @@ return {
 			},
 		},
 	},
-	{
-		"akinsho/bufferline.nvim",
-		init = function()
-			local bufline = require("catppuccin.groups.integrations.bufferline")
-			function bufline.get()
-				return bufline.get_theme()
-			end
-		end,
-	},
 }
