8120c35b03748fd454ae36090b7ed5e7e48e9a16
Bryan Hoang <bryan@bryanhoang.dev>
2025-10-02 00:14:25 -0400

    feat(pwsh): add completions for choco

--- .config/powershell/Initialize-Completions.ps1
+++ .config/powershell/Initialize-Completions.ps1
@@ -28,3 +28,13 @@ if (Test-CommandExists kubectl)
 {
 	(& kubectl completion powershell) | Out-String | Invoke-Expression
 }
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if ($IsWindows && Test-Path($ChocolateyProfile)) {
+	Import-Module "$ChocolateyProfile"
+}
