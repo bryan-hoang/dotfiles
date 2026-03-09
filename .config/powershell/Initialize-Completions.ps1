896c77f4fbf88e8ade77a93b2709d4293d6ccc6a
Bryan Hoang <bryan@bryanhoang.dev>
2026-03-09 09:56:34 -0400

    feat(pwsh): add atuin, mize, & az completions

--- .config/powershell/Initialize-Completions.ps1
+++ .config/powershell/Initialize-Completions.ps1
@@ -36,10 +36,32 @@ Add-Completion 'kubectl' 'kubectl completion powershell'
 Add-Completion 'op' 'op completion powershell'
 # Add-Completion 'sqlcmd' 'sqlcmd completion powershell'
 Add-Completion 'minikube' 'minikube completion powershell'
+Add-Completion 'atuin' 'atuin gen-completions --shell powershell'
+Add-Completion 'mise' 'mise completion powershell'
 
 $completionFiles = Get-ChildItem $PwshCompletionDir -Filter *.ps1
 foreach ($file in $completionFiles) {
 	. $file.FullName
+}
+
+if (Test-CommandExists az) {
+	Register-ArgumentCompleter -Native -CommandName az -ScriptBlock {
+		param($commandName, $wordToComplete, $cursorPosition)
+			$completion_file = New-TemporaryFile
+			$env:ARGCOMPLETE_USE_TEMPFILES = 1
+			$env:_ARGCOMPLETE_STDOUT_FILENAME = $completion_file
+			$env:COMP_LINE = $wordToComplete
+			$env:COMP_POINT = $cursorPosition
+			$env:_ARGCOMPLETE = 1
+			$env:_ARGCOMPLETE_SUPPRESS_SPACE = 0
+			$env:_ARGCOMPLETE_IFS = "`n"
+			$env:_ARGCOMPLETE_SHELL = 'powershell'
+			az 2>&1 | Out-Null
+			Get-Content $completion_file | Sort-Object | ForEach-Object {
+				[System.Management.Automation.CompletionResult]::new($_, $_, "ParameterValue", $_)
+			}
+		Remove-Item $completion_file, Env:\_ARGCOMPLETE_STDOUT_FILENAME, Env:\ARGCOMPLETE_USE_TEMPFILES, Env:\COMP_LINE, Env:\COMP_POINT, Env:\_ARGCOMPLETE, Env:\_ARGCOMPLETE_SUPPRESS_SPACE, Env:\_ARGCOMPLETE_IFS, Env:\_ARGCOMPLETE_SHELL
+	}
 }
 
 # if (Test-CommandExists dotnet) {
