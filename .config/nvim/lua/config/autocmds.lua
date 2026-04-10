a933a84877378255b48ed18a97eda9e2ca34f885
Bryan Hoang <bryan@bryanhoang.dev>
2026-04-10 12:06:43 -0400

    chore(nvim): disable vim's spell check

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,6 +2,9 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- Prefer using other tools for spell checking.
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
 local function augroup(name)
 	return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
 end
