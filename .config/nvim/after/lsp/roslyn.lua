e592072a88b1bcd60dfd1d6d74aedb61a743fedb
Bryan Hoang <bryan@bryanhoang.dev>
2026-04-20 23:44:20 -0400

    feat: migrate to using hks

--- .config/nvim/after/lsp/roslyn.lua
+++
@@ -1,8 +0,0 @@
--- FIXME: https://github.com/seblyng/roslyn.nvim/issues/236#issuecomment-3195101051
-return {
-	handlers = {
-		["workspace/_roslyn_projectNeedsRestore"] = function(_, result, ctx)
-			return vim.NIL
-		end,
-	},
-}
--- .config/nvim/lua/plugins/extras/lang/pkl.lua
+++ .config/nvim/lua/plugins/extras/lang/pkl.lua
@@ -1,24 +1,30 @@
 return {
-	"apple/pkl-neovim",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		{
-			"mason-org/mason.nvim",
-			opts = {
-				ensure_installed = {
-					"pkl-lsp",
+	{
+		"apple/pkl-neovim",
+		ft = "pkl",
+		dependencies = {
+			{
+				"nvim-treesitter/nvim-treesitter",
+				{
+					"mason-org/mason.nvim",
+					opts = {
+						ensure_installed = {
+							"pkl-lsp",
+						},
+					},
 				},
 			},
 		},
+		build = function()
+			require("pkl-neovim").init()
+
+			-- Set up syntax highlighting.
+			vim.cmd("TSInstall pkl")
+		end,
+		init = function()
+			vim.g.pkl_neovim = {
+				start_command = { "pkl-lsp" },
+			}
+		end,
 	},
-	build = function()
-		require("pkl-neovim").init()
-	end,
-	ft = "pkl",
-	init = function()
-		-- Configure pkl-lsp
-		vim.g.pkl_neovim = {
-			start_command = { "pkl-lsp" },
-		}
-	end,
 }
