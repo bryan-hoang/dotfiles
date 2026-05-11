39bbfc9e52f57ffae7de149d2ca69f227c996843
Bryan Hoang <bryan@bryanhoang.dev>
2026-05-11 19:56:01 -0400

    fix(nvim): fix syntax highlighting of .env files

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,9 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		opts = function(_, opts)
-			-- https://github.com/nvim-treesitter/nvim-treesitter/wiki/Windows-support#how-will-the-parser-be-downloaded
-			vim.treesitter.language.register("markdown", "markdown.mdx")
-			vim.treesitter.language.register("bash", "dotenv")
+		opts = function(_, _opts)
+			vim.treesitter.language.register("markdown", { "markdown.mdx" })
+			vim.treesitter.language.register("bash", { "dotenv" })
 		end,
 	},
 }
