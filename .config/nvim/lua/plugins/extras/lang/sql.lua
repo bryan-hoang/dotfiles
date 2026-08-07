df4a63b9187bd08ff8a757ce51a362381de1de1e
Bryan Hoang <bryan@bryanhoang.dev>
2026-08-07 08:25:55 -0400

    chore(git,mise,nvim,hk): ignore agents.md, rm atuin, alias sqlfluff, uninstall sqlfluff, & fix hk in ~

--- .config/nvim/lua/plugins/extras/lang/sql.lua
+++ .config/nvim/lua/plugins/extras/lang/sql.lua
@@ -1,5 +1,13 @@
 return {
 	{
+		"mason-org/mason.nvim",
+		opts = function(_, opts)
+			opts.ensure_installed = vim.tbl_filter(function(p)
+				return not vim.tbl_contains({ "sqlfluff" }, p)
+			end, opts.ensure_installed)
+		end,
+	},
+	{
 		"stevearc/conform.nvim",
 		optional = true,
 		opts = function(_, opts)
